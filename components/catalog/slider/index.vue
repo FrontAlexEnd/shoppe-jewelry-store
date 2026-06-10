922cd79aeade41ab8a0aabb2f0507fd1f5489d70
Bibishev <abibishev007@gmail.com>
2026-06-10 19:42:05 +0700

    feat: migrate main page slider to nuxt and changes styles

--- components/catalog/slider/index.vue
+++ components/catalog/slider/index.vue
@@ -2,34 +2,32 @@
 /* imports for swiper.vue */
 import { Pagination, Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 const SWIPE_SLIDES_MS = 5000
 const SLIDES_SPACE_BETWEEN = 20
 const MAX_WIDTH = 767
 
-const onSwiper = () => {}
-const onSlideChange = () => {}
 const modules = [Pagination, Autoplay]
 
-const fetchSlides = useAsyncData('slider-slides', () => {
-    fetchApi<{ data: Slide[] }>(API.SLIDES)
+const { data: slidesData } = useAsyncData('slider-slides', () => {
+    return fetchApi<{ data: Slide[] }>(API.SLIDES)
 })
-const slides = computed(() => fetchSlides)
-
+const slides = computed(() => slidesData.value?.data ?? [])
 </script>
 
 <template>
   <Swiper
-    :modules= "modules"
+    class="hero__swiper"
+    :modules="modules"
     :space-between="SLIDES_SPACE_BETWEEN"
     :autoplay="{
         delay: SWIPE_SLIDES_MS,
         disableOnInteraction: false
     }"
     :pagination="{ clickable: true }"
-    :loop="true"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange">
+    :loop="true">
     <SwiperSlide 
         v-for="slide in slides" 
         :key="slide.id">
@@ -46,11 +44,11 @@ const slides = computed(() => fetchSlides)
             <div class="hero__body">
                 <h2 class="h2 hero__body-title">{{ slide.product.title }}</h2>
                 <p class="hero__body-pricing">$ {{slide.product.price}},00</p>
-                <RouterLink 
-                    :to="{ name: 'Product', params: { id: slide.product.documentId }}" 
+                <NuxtLink 
+                    :to="`/product/${slide.product.documentId}`" 
                     class="hero__body-button button">
                     View product
-                </RouterLink>
+                </NuxtLink>
             </div>
         </div>
     </SwiperSlide>
